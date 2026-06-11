6a16078a5904e5e3ff9eb482ba0c597ca324df5c
Oriol Orbea <oriol.orbea@gmail.com>
2026-06-11 20:37:16 +0200

    Add docstrings to abstract methods in ICollaborationToolIntegrationService

--- src/mcp_server/application/service_interfaces/collaboration_tool_integration_service_interface.py
+++ src/mcp_server/application/service_interfaces/collaboration_tool_integration_service_interface.py
@@ -9,8 +9,24 @@ class ICollaborationToolIntegrationService(ABC):
 
     @abstractmethod
     async def get_issue(self, issue_id: str, user_id: str) -> CollaborationTask:
-        pass #TODO: Update method return value
+        """
+        Retrieve a specific issue/task from the collaboration tool by its ID.
+
+        Args:
+            issue_id (str): The ID of the issue/task to retrieve.
+            user_id (str): The ID of the user making the request, used for authentication.
+        Returns:
+            A domain model representing the issue/task.
+        """
 
     @abstractmethod
     async def get_pending_issues(self, user_id: str, assignee: str) -> Iterable[CollaborationTask]:
-        pass #TODO: Update method return value
+        """
+        Retrieve all pending issues/tasks from the collaboration tool, filtered by assignee.
+
+        Args:
+            user_id (str): The ID of the user making the request, used for authentication.
+            assignee (str): The username of the assignee to filter issues by.
+        Returns:
+            An iterable of domain models representing the pending issues/tasks.
+        """
